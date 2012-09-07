b59484aaef49a59bd760cdfde242023d852c5d70
Geoff Hayes <hayesgm@gmail.com>
2012-09-06 17:11:07 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ego::Application.config.secret_token = '2423be2ae6f1e39549e1c150d37a30712b327f6596baa1a04ad623154b5262374287ae02a3c1f15439d61f734cf888792c01a97f4d611aacf765c009da436158'
